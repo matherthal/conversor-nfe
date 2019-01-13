44205d71146b2fe82258cb6c9b0399486a289c5a
= <=>
2019-01-13 03:16:54 -0200

    Add new fields

--- conversor-nfe/conversor-nfe.py
+++ conversor-nfe/conversor-nfe.py
@@ -13,8 +13,9 @@ def process_nfes(local):
     parsed = [_parse_xml(f) for f in _fetch_xml_files(local)]
 
     with open('output.tsv', mode='w') as csv_file:
-        fieldnames = ['caminho', 'xNome', 'cProd', 'cEAN', 'xProd',
-                      'NCM', 'cEANTrib', 'CEST', 'orig', 'cProdANVISA']
+        fieldnames = ['caminho', 'xNome', 'cProd', 'cEAN', 'xProd', 'NCM', 'cEANTrib', 'CEST', 
+                      'cProdANVISA', 'CFOP', 'uCom', 'qCom', 'vUnCom', 'vProd', 'vTotTrib', 
+                      'orig', 'CST', 'vBCSTRet', 'pST', 'vICMSSTRet']
         
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter='\t')
         writer.writeheader()
@@ -53,6 +54,8 @@ def _parse_xml(path):
         emit = inf.find('ns:emit', ns)
         # dest = inf.find('ns:dest', ns)
 
+        get_optional = lambda v: v.text if v else None
+
         if inf is None:
             return None
 
@@ -61,39 +64,62 @@ def _parse_xml(path):
         for det in inf.findall('ns:det', ns):
             xNome = emit.find('ns:xNome', ns).text
             
-            #Atribubtos de prod
-            for prod in det.findall('ns:prod', ns):
-                cProd = prod.find('ns:cProd', ns).text
-                cEAN = prod.find('ns:cEAN', ns)
-                if cEAN is not None:
-                    cEAN = cEAN.text
-                xProd = prod.find('ns:xProd', ns).text
-                NCM = prod.find('ns:NCM', ns).text
-                cEANTrib = prod.find('ns:cEANTrib', ns).text
-                CEST = prod.find('ns:CEST', ns)
-                if CEST is not None:
-                    CEST = CEST.text
-                cProdANVISA = prod.find('ns:med/ns:cProdANVISA', ns)
-                if cProdANVISA:
-                    cProdANVISA = cProdANVISA.text
+            # Processing attributes of prod
+            prod = det.find('ns:prod', ns)
 
-                for node in det.find('ns:imposto/ns:ICMS', ns).findall('*'):
-                    orig = node.find('ns:orig', ns).text
-                
-                nfe = {
-                    'caminho': path,
-                    'xNome': xNome,
-                    'cProd': cProd,
-                    'cEAN': cEAN,
-                    'xProd': xProd,
-                    'NCM': NCM,
-                    'cEANTrib': cEANTrib,
-                    'CEST': CEST,
-                    'orig': orig,
-                    'cProdANVISA': cProdANVISA
-                }
-                
-                nfes.append(nfe)
+            cProd = prod.find('ns:cProd', ns).text
+            cEAN = get_optional(prod.find('ns:cEAN', ns))
+            xProd = prod.find('ns:xProd', ns).text
+            NCM = prod.find('ns:NCM', ns).text
+            CEST = get_optional(prod.find('ns:CEST', ns))
+            CFOP = prod.find('ns:CFOP', ns).text
+            uCom = prod.find('ns:uCom', ns).text
+            qCom = prod.find('ns:qCom', ns).text
+            vUnCom = prod.find('ns:vUnCom', ns).text
+            vProd = prod.find('ns:vProd', ns).text
+            cEANTrib = get_optional(prod.find('ns:cEANTrib', ns))
+            cProdANVISA = get_optional(prod.find('ns:med/ns:cProdANVISA', ns))
+
+            # Processing attributes of imposto
+            imposto = det.find('ns:imposto', ns)
+            vTotTrib = imposto.find('ns:vTotTrib', ns).text
+            
+            inner_icms = imposto.find('ns:ICMS', ns).findall('*')
+            if len(inner_icms) > 1: raise Exception('Múltiplos campos dentro de ICMS')
+            inner_icms = inner_icms[0]
+
+            orig = inner_icms.find('ns:orig', ns).text
+            CST = inner_icms.find('ns:CST', ns).text
+            vBCSTRet = get_optional(inner_icms.find('ns:vBCSTRet', ns))
+            pST = get_optional(inner_icms.find('ns:pST', ns))
+            vICMSSTRet = get_optional(inner_icms.find('ns:vICMSSTRet', ns))
+
+            nfe = {
+                'caminho': path,
+                'xNome': xNome,
+                # Produto
+                'cProd': cProd,
+                'cEAN': cEAN,
+                'xProd': xProd,
+                'NCM': NCM,
+                'cEANTrib': cEANTrib,
+                'CEST': CEST,
+                'cProdANVISA': cProdANVISA,
+                'CFOP': CFOP,
+                'uCom': uCom,
+                'qCom': qCom,
+                'vUnCom': vUnCom,
+                'vProd': vProd,
+                # Imposto
+                'vTotTrib': vTotTrib,
+                'orig': orig,
+                'CST': CST,
+                'vBCSTRet': vBCSTRet,
+                'pST': pST,
+                'vICMSSTRet': vICMSSTRet,
+            }
+            
+            nfes.append(nfe)
                 
         return nfes
     except:
